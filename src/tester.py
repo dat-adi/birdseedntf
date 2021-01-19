68651958bbbc8a9448e6eba17eeaa672b3fea16f
dat-adi <dat.adithya@gmail.com>
2021-01-19 10:33:19 +0530

    Added ignore, requirements, and src

---
+++ src/tester.py
@@ -0,0 +1,19 @@
+import tweepy
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+load_dotenv()
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
+
+print("Just a notification")
+
+CONSUMER_KEY = os.getenv("API_KEY")
+CONSUMER_SECRET = os.getenv("API_KEY_SECRET")
+ACCESS_KEY = os.getenv("ACCESS_TOKEN")
+ACCESS_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
