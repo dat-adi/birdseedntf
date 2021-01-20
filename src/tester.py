5db9d3922a75fb9ee27bd76c9e4621e2e1eb77b1
dat-adi <dat.adithya@gmail.com>
2021-01-20 15:43:25 +0530

    Added mention.user details

--- src/tester.py
+++ src/tester.py
@@ -17,3 +17,12 @@ ACCESS_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
+
+mentions = api.mentions_timeline()
+
+for mention in mentions:
+    print(str(mention.id) + " - " + mention.text)
+    if '#remindmein' in mention.text.lower():
+        print("Found #remindmein")
+        print(mention.user.__dict__['name'])
+        print("Responding back...")
